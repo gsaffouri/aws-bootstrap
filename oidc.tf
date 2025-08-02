4f78655adb984ff2e2c17412731dcedc148a1cb6
George Saffouri <george.saffouri@outlook.com>
2025-08-02 13:19:40 -0400

    wip

--- oidc.tf
+++ oidc.tf
@@ -60,9 +60,53 @@ resource "aws_iam_policy" "oidc_admin_policy" {
 }
 
 # Attach the OIDC Admin policy to the role
+# This allows the role to perform OIDC management tasks
+# Ensure that the role is assumed by the correct user or role that runs Terraform
 resource "aws_iam_role_policy_attachment" "attach_oidc_admin_policy" {
   role       = aws_iam_role.terraform_oidc_admin.name
   policy_arn = aws_iam_policy.oidc_admin_policy.arn
+}
+
+resource "aws_iam_policy" "assume_oidc_admin_role_policy" {
+  name = "AssumeTerraformOIDCAdminRole"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "sts:AssumeRole",
+        Resource = aws_iam_role.terraform_oidc_admin.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "allow_cloud_user_assume_role" {
+  user       = "cloud_user"
+  policy_arn = aws_iam_policy.assume_oidc_admin_role_policy.arn
+}
+
+resource "aws_iam_policy" "cloud_user_get_oidc_provider" {
+  name = "AllowGetOIDCProvider"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "iam:GetOpenIDConnectProvider"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "cloud_user_oidc_read_attach" {
+  user       = "cloud_user"
+  policy_arn = aws_iam_policy.cloud_user_get_oidc_provider.arn
 }
 
 
