e149cf90a430b51381f871396169257060e11cdf
George Saffouri <george.saffouri@outlook.com>
2025-08-02 11:16:13 -0400

    wip

--- oidc.tf
+++ oidc.tf
@@ -3,7 +3,7 @@ module "oidc-github" {
   version = "1.6.0"
 
   github_repositories = [
-    "your-gh-org-or-user/your-eks-repo:ref:refs/heads/main"
+    "gsaffouri/eks-deployment:ref:refs/heads/main"
   ]
 
   attach_admin_policy = true # or false if you're defining fine-grained IAM later 
