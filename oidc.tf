1ace47d46fa9b7a8840bdde98651dc6b24e714f9
George Saffouri <george.saffouri@outlook.com>
2025-08-05 18:30:24 -0400

    wip

--- oidc.tf
+++ oidc.tf
@@ -1,6 +1,6 @@
 module "oidc-github" {
   source  = "unfunco/oidc-github/aws"
-  version = "1.6.0"
+  version = "1.8.1"
 
   github_repositories = [
     "gsaffouri/eks-deployment:ref:refs/heads/main"
