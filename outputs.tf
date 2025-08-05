f11e960008e57a47fea5f7107d4617546a15c81d
George Saffouri <george.saffouri@outlook.com>
2025-08-05 18:36:00 -0400

    wip

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,15 @@
 # Outputs for the Terraform configuration
 
 output "vpc_id" {
-  description = "The ID of the created VPC"
+  description = "VPC ID"
   value       = module.vpc.vpc_id
 }
 
-output "private_subnet_ids" {
+output "private_subnets" {
   description = "List of private subnet IDs"
   value       = module.vpc.private_subnets
 }
+
 
 output "public_subnet_ids" {
   description = "List of public subnet IDs"
