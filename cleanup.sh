339d0f69f378aaac149d872da7f51e7535456933
root <root@LAP-GORILLA>
2025-08-06 13:00:02 -0400

    adding cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+# Cleans up various files to prepare for a fresh deployment and prevent conflicts w/ the state file.
+rm -fr .terraform .terraform.lock.hcl terraform.tfstate terraform.tfstate.backup main.tf
